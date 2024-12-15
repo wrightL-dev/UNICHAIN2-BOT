967170c64be71413c3a47a6e84826708f8c21bf6
wrightL <anon2pai@gmail.com>
2024-12-15 19:08:49 +0700

    Update main.js

--- main.js
+++ main.js
@@ -11,8 +11,9 @@ function clearConsole() {
   console.log("\x1b[38;5;117mGITHUB: https://github.com/wrightL-dev\x1b[0m");
   console.log("\x1b[38;5;159mTELEGRAM CHANNEL: https://t.me/tahuri01\x1b[0m");
   console.log("==================== BOT UNICHAIN ====================");
-  console.log("\x1b[38;5;82m[✔️ ] BOT STATUS: \x1b[38;5;46mRUNNING\x1b[0m");
+  console.log("\x1b[38;5;82m[ ✔️ ] BOT STATUS: \x1b[38;5;46mRUNNING\x1b[0m");
   console.log("======================================================");
+  console.log("\x1b[38;5;214m[ ⚠️ ] NOTE: Jangan lupa /start dahulu di bot sebelum menjalankan ini!\x1b[0m");
 }
 
 function runScripts(scripts) {
